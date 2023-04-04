83c1387ebcbf98c722faa1ef6a7e9ca037b8c25a
Victor Samyr <vssa1@aluno.ifal.edu.br>
2023-04-04 14:42:04 -0300

    Agora tem o robo amarelo, e números com duas casas decimas

--- robotron-2000-projeto_inicial/js/script.js
+++ robotron-2000-projeto_inicial/js/script.js
@@ -1,4 +1,4 @@
-const colors = ['robotron.png','robotron-vermelho.png','robotron-rosa.png','robotron-preto.png','robotron-branco.png','robotron-azul.png','robotron-amarelo.png']
+const colors = ['robotron-vermelho.png','robotron-rosa.png','robotron-preto.png','robotron-branco.png','robotron-azul.png','robotron-amarelo.png']
 let colorIndex = 0
 
 function changeColor(x){
@@ -7,10 +7,12 @@ function changeColor(x){
         colorIndex = colorIndex + 1 > 5 ? -1 : colorIndex
         robo.src = 'img/'+ colors[colorIndex+x]
         colorIndex++
+		console.log(colorIndex)
     }else{
         colorIndex = colorIndex - 1 < 0 ? 6 : colorIndex
         robo.src = 'img/'+ colors[colorIndex+x]
         colorIndex--
+		console.log(colorIndex)
     }
 }
 
@@ -18,9 +20,9 @@ const controllers = document.getElementsByClassName('controle-contador')
 function changeContentValues(x, y){
     let value = Number(controllers[x].value)
     if(y == "+"){
-        controllers[x].value = value + 1 <= 10 ? value + 1 : value
+        controllers[x].value = value + 1 <= 10 ? String(value + 1).padStart(2,"0") : value
     }else{
-        controllers[x].value = value - 1 >= 0 ? value - 1 : value
+        controllers[x].value = value - 1 >= 0 ? String(value + 1).padStart(2,"0") : value
     }
 }
 
